f6d57f3bec98e03f1331386e448db282cf07a3ed
mrbsantiago <mrbsantiago@gmail.com>
2021-01-15 06:50:13 -0400

    add basic functionality

--- quote_generator/script.js
+++ quote_generator/script.js
@@ -1,1 +1,57 @@
-console.log("");
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  // Check if author is blank and replace with 'unknown'
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  // Check quote length to determine styling
+  if (quote.text.length > 50) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+}
+
+// Get Quotes From API
+
+async function getQuote() {
+  const apiURL = "https://type.fit/api/quotes";
+
+  try {
+    const response = await fetch(apiURL);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+    console.log("whoops, no quote ", error);
+  }
+}
+
+// Tweet a quote
+
+function tweetQuote() {
+  const twitterURL = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterURL, "_blank"); // Opens the URL in a new tab
+}
+
+// Event listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// On Load
+getQuote();
