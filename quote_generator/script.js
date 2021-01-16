a25b9d2e2284e78febcb86b0a5c7af405257424f
mrbsantiago <mrbsantiago@gmail.com>
2021-01-16 06:42:35 -0400

    add final feature

--- quote_generator/script.js
+++ quote_generator/script.js
@@ -3,11 +3,26 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+//Show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//Hide Loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote() {
+  loading();
+
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -24,12 +39,16 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
+
+  // Set quote, hide loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes From API
 
 async function getQuote() {
+  loading();
   const apiURL = "https://type.fit/api/quotes";
 
   try {
